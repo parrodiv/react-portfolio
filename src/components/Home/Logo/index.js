98b58d969869f4eeb4639014c33e63f3a873b46f
Alessandro Parrilla <paggio96@gmail.com>
2022-07-07 14:58:00 +0200

    Logo animation without svg and gsap for now, I will implement something with my photo or something else

---
+++ src/components/Home/Logo/index.js
@@ -0,0 +1,14 @@
+import './index.scss'
+import LogoS from '../../../assets/images/logo-s.png'
+
+const Logo = () => {
+  
+  return (
+    <div className="logo-container">
+      <img src={LogoS} alt="S" className="solid-logo" />
+      
+    </div>
+  )
+}
+
+export default Logo
