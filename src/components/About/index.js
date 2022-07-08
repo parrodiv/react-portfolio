a65db105f4b7d4bf0569e2e02e713e6d67e458e2
Alessandro Parrilla <paggio96@gmail.com>
2022-07-08 12:38:33 +0200

    Animation skills cube finished

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,3 +1,5 @@
+import { faBootstrap, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState, useEffect } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
@@ -39,6 +41,29 @@ const About = () => {
           molestias dolore debitis explicabo. Doloremque possimus ipsum
           voluptatibus atque delectus, necessitatibus accusantium!
         </p>
+      </div>
+
+      <div className="stage-cube-cont">
+        <div className="cubespinner">
+          <div className="face1">
+            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+          </div>
+          <div className="face2">
+            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+          </div>
+          <div className="face3">
+            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+          </div>
+          <div className="face4">
+            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+          </div>
+          <div className="face5">
+            <FontAwesomeIcon icon={faGitAlt} color="#EFD81D" />
+          </div>
+          <div className="face6">
+            <FontAwesomeIcon icon={faBootstrap} color="#653D7C" />
+          </div>
+        </div>
       </div>
     </div>
   )
