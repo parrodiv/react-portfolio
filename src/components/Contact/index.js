0ecc6319ae68799bd8ea82c2fe9aa187d1b45ad3
Alessandro Parrilla <paggio96@gmail.com>
2022-07-11 11:42:11 +0200

    Added Leaflet Map

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,6 +3,7 @@ import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -38,7 +39,7 @@ const Contact = () => {
           <h1>
             <AnimatedLetters
               letterClass={letterClass}
-              strArray={['C', 'o', 'n', 't', 'a', 'c', 't',' ', 'm', 'e']}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
               idx={15}
             />
           </h1>
@@ -86,6 +87,25 @@ const Contact = () => {
               </ul>
             </form>
           </div>
+        </div>
+        <div className='info-map'>
+          Alessandro Parrilla,
+          <br />
+          Italia,
+          <br />
+          Via Odino Braglia 8, 46023 <br />
+          Gonzaga 46023 MN <br />
+          <span>alessandro.parrilla.dev@gmail.com</span>
+        </div>
+        <div className='map-wrap'>
+          <MapContainer center={[44.951681, 10.819714]} zoom={13}>
+            <TileLayer
+              url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+            />
+            <Marker position={[44.951681, 10.819714]}>
+              <Popup>Alessandro lives here :)</Popup>
+            </Marker>
+          </MapContainer>
         </div>
       </div>
       <Loader type='pacman' />
