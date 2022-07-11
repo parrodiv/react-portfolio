dc6f978ea62810e4b6266d4514957e9c9a0d9747
Alessandro Parrilla <paggio96@gmail.com>
2022-07-11 15:11:47 +0200

    Bulding form on Dashboard component

--- src/components/Dashboard/home.js
+++ src/components/Dashboard/home.js
@@ -1,9 +1,33 @@
-
+import { useRef } from "react"
+import { auth } from "../../firebase.config"
 
 const Home = () => {
+  const form = useRef()
+
+  const submitPortfolio = (e) => {
+    e.preventDefault()
+    // otteniamo gli input grazie al useRef, da tenere conto che l'ordine 
+    // con cui vengono presi i dati dal form è quello presentato nel form (name, description, url, image)
+    const name = form.current[0]?.value 
+    //?. opt chaining operator per far in modo di non bloccare il codice nel caso in cui sia null o undefined restituendo direttamente undefined
+    const description = form.current[1]?.value
+    const url = form.current[2]?.value
+    const image = form.current[3]?.files[0]
+
+    console.log(name, description, url, image);
+  }
+
   return (
     <div className="dashboard">
-      Hello
+      <form ref={form} onSubmit={submitPortfolio}>
+        <p><input type="text" placeholder="Name" /></p>
+        <p><textarea placeholder="Description" /></p>
+        <p><input type="text" placeholder="Url" /></p>
+        <p><input type="file" placeholder="Image" /></p>
+        <button type="submit">Submit</button>
+        <button onClick={() => auth.signOut()}>Sign Out</button>
+        
+      </form>
     </div>
   )
 }
